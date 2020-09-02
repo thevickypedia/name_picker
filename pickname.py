2f8339a0b5d64a5c6aae513914a9b4cdad3d68af
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-02 10:05:45 -0500

    remove first 18 lines in the file instead of deleting the file

--- pickname.py
+++ pickname.py
@@ -2,8 +2,30 @@ import random
 import os
 
 
+def new_file():
+    new_names = names
+    name_file = os.path.isfile('names.txt')
+    if name_file:
+        req_file = open('names.txt', 'r')
+        if len(list(req_file)) == len(names):
+            n = 18
+            nfirstlines = []
+            with open("names.txt") as f, open("namestmp.txt", "w") as out:
+                for x in range(n):
+                    nfirstlines.append(next(f))
+                for line in f:
+                    out.write(line)
+            os.remove("names.txt")
+            os.rename("namestmp.txt", "names.txt")
+        f = open('names.txt', 'r')
+        for item in f:
+            item = item.replace('\n', '')
+            new_names.remove(item)
+    return new_names
+
+
 def check_file():
-    name = random.choice(new_names)
+    name = random.choice(new_file())
     file = open('names.txt', 'a')
     file.write(f'{name}\n')
     file.close()
@@ -14,15 +36,4 @@ if __name__ == '__main__':
     names = ['Iron Man', 'Captain America', 'Hulk', 'Thor Odinson', 'Black Widow', 'Hawkeye', 'War Machine',
              'Ant-Man', 'Doctor Strange', 'Black Panther', 'Captain Marvel', 'Spider-Man', 'Nebula', 'Gamora',
              'Wasp', 'Jarvis', 'Groot', 'Rocket', 'Thanos', 'Nick Fury', 'Friday']
-    new_names = names
-    name_file = os.path.isfile('names.txt')
-    if name_file:
-        fil = open('names.txt', 'r')
-        for item in fil:
-            item = item.replace('\n', '')
-            new_names.remove(item)
-        if len(list(fil)) == len(names):
-            os.remove('names.txt')
-            print('File has been removed after choosing all team members. Re-run the script for a fresh start.')
-            exit(0)
     print(check_file())
