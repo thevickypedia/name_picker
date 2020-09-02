07e798c1cc662fc65d195945bd555e67240f820a
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-02 10:11:50 -0500

    remove 70% of the names from name list once the list has been exhausted

--- pickname.py
+++ pickname.py
@@ -1,18 +1,21 @@
-import random
 import os
+import random
 
 
-def new_file():
-    new_names = names
+def new_list():
+    new_name_list = name_list
     name_file = os.path.isfile('names.txt')
     if name_file:
         req_file = open('names.txt', 'r')
-        if len(list(req_file)) == len(names):
-            n = 18
-            nfirstlines = []
+        file_length = len(list(req_file))
+        if file_length == len(name_list):
+            n = int(len(name_list) - ((30 * file_length) / 100))
+            print(f'Removed {n} lines from names.txt as all the names in the list have been picked at least '
+                  f'once.')
+            first_lines = []
             with open("names.txt") as f, open("namestmp.txt", "w") as out:
                 for x in range(n):
-                    nfirstlines.append(next(f))
+                    first_lines.append(next(f))
                 for line in f:
                     out.write(line)
             os.remove("names.txt")
@@ -20,12 +23,12 @@ def new_file():
         f = open('names.txt', 'r')
         for item in f:
             item = item.replace('\n', '')
-            new_names.remove(item)
-    return new_names
+            new_name_list.remove(item)
+    return new_name_list
 
 
 def check_file():
-    name = random.choice(new_file())
+    name = random.choice(new_list())
     file = open('names.txt', 'a')
     file.write(f'{name}\n')
     file.close()
@@ -33,7 +36,7 @@ def check_file():
 
 
 if __name__ == '__main__':
-    names = ['Iron Man', 'Captain America', 'Hulk', 'Thor Odinson', 'Black Widow', 'Hawkeye', 'War Machine',
-             'Ant-Man', 'Doctor Strange', 'Black Panther', 'Captain Marvel', 'Spider-Man', 'Nebula', 'Gamora',
-             'Wasp', 'Jarvis', 'Groot', 'Rocket', 'Thanos', 'Nick Fury', 'Friday']
+    name_list = ['Iron Man', 'Captain America', 'Hulk', 'Thor Odinson', 'Black Widow', 'Hawkeye', 'War Machine',
+                 'Ant-Man', 'Doctor Strange', 'Black Panther', 'Captain Marvel', 'Spider-Man', 'Nebula', 'Gamora',
+                 'Wasp', 'Jarvis', 'Groot', 'Rocket', 'Thanos', 'Nick Fury', 'Friday']
     print(check_file())
