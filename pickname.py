03b113a97ce4e1cb85efa8d7ff06e56faa9dafa8
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-02 09:57:38 -0500

    pick a random name from given list

---
+++ pickname.py
@@ -0,0 +1,7 @@
+import random
+
+names = ['Iron Man', 'Captain America', 'Hulk', 'Thor Odinson', 'Black Widow', 'Hawkeye', 'War Machine',
+         'Ant-Man', 'Doctor Strange', 'Black Panther', 'Captain Marvel', 'Spider-Man', 'Nebula', 'Gamora',
+         'Wasp', 'Jarvis', 'Groot', 'Rocket', 'Thanos', 'Nick Fury', 'Friday']
+name = random.choice(names)
+print(name)
